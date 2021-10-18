c29c0e83abac75282eef85422daeb78dfa94199a
Shishir Shekhar <71517975+ShishirShekhar@users.noreply.github.com>
2021-10-18 22:07:44 +0530

    added lang, star and fork

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Import necessary modules
+import numpy as np
 import requests
 from bs4 import BeautifulSoup as bs
 
@@ -21,6 +22,9 @@ block = soup.find_all("article", {"class": "Box-row"})
 # Create list of different details
 author = []
 repo = []
+lang = []
+star = []
+fork = []
 
 # Iterate through each block of article tag
 for i in range(len(block)):
@@ -34,7 +38,43 @@ for i in range(len(block)):
     author.append(value_list[0])
     repo.append(value_list[1][2:])
 
-# Print author and repo
+    # Get the values language that is in span tag
+    lang_list = block[i].find_all("span", {"itemprop": "programmingLanguage"})
+    # Check if lang_list available
+    if (lang_list):
+        lang.append(lang_list[0].text)
+    else:
+        lang.append(np.nan)
+
+    # Get the values star that is in a tag
+    star_list = block[i].find_all("a", {"class": "Link--muted d-inline-block mr-3"})
+    # Check if star_list available
+    if (star_list):
+        # Remove extra spaceses from the text values
+        star.append(star_list[0].text.strip())
+    else:
+        star.append(np.nan)
+
+    # Get the values fork that is in a tag
+    fork_list = block[i].find_all("a", {"class": "Link--muted d-inline-block mr-3"})
+    # Check if star_list available
+    if (fork_list):
+        # Remove extra spaceses from the text values
+        fork.append(fork_list[1].text.strip())
+    else:
+        fork.append(np.nan)
+
+
+
+# Print details of the treding repos
+print("\n")
+print("Details of Trending Repos")
+print("*" * 50)
 for i in range(len(author)):
-    print(author[i], "==>", repo[i])
-    print("-"*50)
+    print("Author   ==>", author[i])
+    print("Repo     ==>", repo[i])
+    print("Language ==>", lang[i])
+    print("Star     ==>", star[i])
+    print("Fork     ==>", fork[i])
+    print("-" * 50)
+    print("\n")
